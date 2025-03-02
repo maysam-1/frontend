6e0b5231982e68f0b38f23c330e190a72c915297
maysam-1 <maysamkh44@gmail.com>
2025-03-02 13:56:29 +0200

    added routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Routes,Route, Router} from "react-router-dom"
+import AllTasks from './components/TaskList';
+import Task from './components/TaskItem';
+import TaskForm from './components/TaskForm';
+import Sidebar from './components/sideBar';
+import TopBar from './components/topBar';
+import KK from './pages/KK';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <BrowserRouter>
+     <TopBar/>
+     <Sidebar/>
+     <Routes>
+      <Route path='task' element={<Task/>}></Route>
+      {/* <Route path='add' element={<TaskForm/>}></Route> */}
+      {/* <Route path="/" element={<Home />} /> */}
+      <Route path="/all-tasks" element={<AllTasks />} />
+      <Route path="/add-task" element={<TaskForm />} />
+      <Route path="/KK" element={<KK/>} />
+
+     </Routes>
+     </BrowserRouter>
     </div>
   );
 }
+
 
 export default App;
