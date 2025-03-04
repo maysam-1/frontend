54121640f729cf1376cb90df456ef28c94565352
maysam-1 <maysamkh44@gmail.com>
2025-03-04 10:13:16 +0200

    added some style and reload after deleting

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -7,7 +7,8 @@ async function fetchTasks() {
   }
 
   async function deleteTask(id) {
-    const res = await axios.put(`http://localhost:8000/delete/${id}`);
+    const res = await axios.put(`http://localhost:8000/tasks/delete/${id}`);
+    window.location.reload();
     return res.data; 
   }
 
@@ -40,13 +41,16 @@ function AllTasks(){
           top:"23px",
           fontSize:"30px",
           fontWeight:"bold"}}>{task.title}</div>
-        <h4 style={{marginLeft:"5px",marginBottom:"1px"}}>due date: {task.dueDate}</h4>
+        <h4 style={{marginLeft:"1px",marginBottom:"1px"}}>due date: {task.dueDate}</h4>
         <button className="delete-btn" onClick={() => deleteTask(task.id)}>Move to Recycle Bin</button>
         <button className="delete-btn">Delete Forever</button>
-
-                <h6 style={{ color: "#3d3f72", marginLeft: "1px"}}>
+          <div style={{display:"flex", flexDirection:"row"}}>
+                <h6 style={{ color: "#3d3f72", marginLeft: "1px",marginRight:"4px"}}>
           {task.isPublic==1 ? "Public" : "Private"}
-        </h6>
+        </h6> 
+          <h6 style={{ color: "#3d3f72", marginLeft: "1px" }}>Priority: 
+            {task.priority_id === 1 ? "High" : task.priority_id === 2 ? "Medium" : "Low"}
+          </h6></div>
         {console.log(`title:${task.title} id:${task.id}`)}
         </div>
     </li>
