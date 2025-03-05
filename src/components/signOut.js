88c2bafbdedbfd926b19f0adcccae52625905f5a
maysam-1 <maysamkh44@gmail.com>
2025-03-05 09:49:15 +0200

    changed the link in the sign out

--- src/components/signOut.js
+++ src/components/signOut.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const handleSignOut = () => {
     localStorage.removeItem("user");
 
-    <Link to="/signup">Sign up</Link>
+    <Link to="/signin">Sign out</Link>
 };
 
 export default handleSignOut;
