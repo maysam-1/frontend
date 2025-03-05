c0d9a80555adc89542b61e6724f8fb1978b590db
maysam-1 <maysamkh44@gmail.com>
2025-03-05 19:14:48 +0200

    removed the delete button and function from Tasklist

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -6,21 +6,19 @@ async function fetchTasks() {
     return res.data; 
   }
 
-  async function deleteTask(id) {
-    const res = await axios.put(`http://localhost:8000/tasks/delete/${id}`);
-    window.location.reload();
-    return res.data; 
-  }
+
 
 
 function AllTasks(){
 
   const { data: tasks = [], error, isLoading } = useQuery({
-    queryKey: ["tasks"], 
+    queryKey: ["alltasks"], 
     queryFn: () => fetchTasks(),
     staleTime: 5000,
   });
-   isLoading&&<h1>is loading...</h1>
+ 
+
+   
    error&&console.log(error.message)
    return(
     <ul className="item-list">
@@ -42,15 +40,17 @@ function AllTasks(){
           fontSize:"30px",
           fontWeight:"bold"}}>{task.title}</div>
         <h4 style={{marginLeft:"1px",marginBottom:"1px"}}>due date: {task.dueDate}</h4>
-        <button className="delete-btn" onClick={() => deleteTask(task.id)}>Move to Recycle Bin</button>
-        <button className="delete-btn">Delete Forever</button>
           <div style={{display:"flex", flexDirection:"row"}}>
                 <h6 style={{ color: "#3d3f72", marginLeft: "1px",marginRight:"4px"}}>
           {task.isPublic==1 ? "Public" : "Private"}
         </h6> 
-          <h6 style={{ color: "#3d3f72", marginLeft: "1px" }}>Priority: 
+          <h6 style={{ color: "#3d3f72", marginLeft: "1px" }}>Priority:-
             {task.priority_id === 1 ? "High" : task.priority_id === 2 ? "Medium" : "Low"}
+          </h6>
+          <h6 style={{ color:"#D5006D", marginLeft: "1px" }}>By:-
+            {task.username}
           </h6></div>
+          
         {console.log(`title:${task.title} id:${task.id}`)}
         </div>
     </li>
