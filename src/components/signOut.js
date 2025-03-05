490dae8d9939c9b91e17fef03590c37c537d598b
maysam-1 <maysamkh44@gmail.com>
2025-03-05 19:12:45 +0200

    added invalidateQueries and reload window to empty the cashe of mytasks and ensure getting the right userid

--- src/components/signOut.js
+++ src/components/signOut.js
@@ -1,10 +1,25 @@
-
 import { Link } from "react-router-dom";
+import { useQueryClient } from "@tanstack/react-query";
 
-const handleSignOut = () => {
-    localStorage.removeItem("user");
+const HandleSignOut = () => {
+  const queryClient = useQueryClient();
 
-    <Link to="/signin">Sign out</Link>
+  const handleSignOut = () => {
+    try {
+      localStorage.removeItem("user");
+      
+      queryClient.invalidateQueries(["mytasks"]);
+      window.location.reload();
+
+    } catch (err) {
+      console.log("Sign out error:", err);
+    }
+  };
+  return (
+    <h5>
+      <Link onClick={handleSignOut} to="/signin">Sign out</Link>
+    </h5>
+  );
 };
 
-export default handleSignOut;
+export default HandleSignOut;
