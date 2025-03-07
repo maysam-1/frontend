b5664ad9718d68ae98a3deeeaf8e71018679f54c
maysam-1 <maysamkh44@gmail.com>
2025-03-07 23:48:09 +0200

    added an icon for view users button

--- src/components/sideBar.jsx
+++ src/components/sideBar.jsx
@@ -6,18 +6,19 @@ import mineGif from "../icons/mine.gif";
 import addGif from "../icons/add2.gif";
 import castleGif from "../icons/castle.gif"
 import HandleSignOut from "./signOut";
+import usersGif from "../icons/users.gif"
 function Sidebar() {
   const [active, setActive] = useState("Home");
   const [collapsed, setCollapsed] = useState(false);
   const navigate = useNavigate(); // Get the navigate function
 
   const menuItems = [
-    { name: "Home", icon: <img src={homeGif} alt="Icon" style={{ height: "2rem", width: "2rem" }} />, path: "/" }, 
+    { name: "Home", icon: <img src={homeGif} alt="Icon" style={{ height: "2rem", width: "2rem" }} />, path: "/home" }, 
     { name: "All Tasks", icon: <img src={pileGif} alt="Icon" style={{ height: "2rem", width: "2rem" }} />, path: "/all-tasks" }, 
     { name: "My Tasks", icon: <img src={mineGif} alt="Icon" style={{ height: "2rem", width: "2rem" }} />, path: "/mytasks" }, 
     { name: "Add Task", icon: <img src={addGif} alt="Icon" style={{ height: "2rem", width: "2rem" }} />, path: "/add-task" }, 
     { name: "THE OGs", icon: <img src={castleGif} alt="Icon" style={{ height: "2rem", width: "2rem" }} />, path: "/KK" }, 
-    { name: "View Users", icon: <img src={homeGif} alt="Icon" style={{ height: "2rem", width: "2rem" }} />, path: "/view-users" } // New button
+    { name: "View Users", icon: <img src={usersGif} alt="Icon" style={{ height: "2rem", width: "2rem" }} />, path: "/view-users" } // New button
 
 
   ];
