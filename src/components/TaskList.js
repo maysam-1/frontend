c3b8edbd7912a0558db1fbc3e011132e05d9e0f8
maysam-1 <maysamkh44@gmail.com>
2025-03-07 17:44:16 +0200

    added error handling try and catch

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,10 +2,14 @@ import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 
 async function fetchTasks() {
+  try {
     const res = await axios.get("http://localhost:8000/tasks/alltasks");
-    return res.data; 
+    return res.data;
+  } catch (error) {
+    console.error("Error fetching tasks:", error);
+    throw new Error("Failed to fetch tasks. Please try again later."); 
   }
-
+}
 
 
 
@@ -19,7 +23,13 @@ function AllTasks(){
  
 
    
-   error&&console.log(error.message)
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
    return(
     <ul className="item-list">
    {
