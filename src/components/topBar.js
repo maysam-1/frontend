080885f51da76b41fbc8d02ffc15d1fd5ef2fb2c
maysam-1 <maysamkh44@gmail.com>
2025-03-07 23:49:32 +0200

    remved some unnecesery notes and import

--- src/components/topBar.js
+++ src/components/topBar.js
@@ -1,12 +1,7 @@
 import React from "react";
-import {useLocation } from "react-router-dom";
 
 function TopBar() {  
-  const location = useLocation();
-
-  // const activeUserId = location.pathname.startsWith("/users/")
-  //   ? parseInt(location.pathname.split("/users/")[1], 10)
-  //   : null;
+ 
 
   return (
     <div>
