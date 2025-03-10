f7c70d60f664af2612c823987efa39e9820cd45d
maysam-1 <maysamkh44@gmail.com>
2025-03-10 13:55:14 +0200

    changed the equal sign to ===

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -52,7 +52,7 @@ function AllTasks(){
         <h4 style={{marginLeft:"1px",marginBottom:"1px"}}>due date: {task.dueDate}</h4>
           <div style={{display:"flex", flexDirection:"row"}}>
                 <h6 style={{ color: "#3d3f72", marginLeft: "1px",marginRight:"4px"}}>
-          {task.isPublic==1 ? "Public" : "Private"}
+          {task.isPublic===1 ? "Public" : "Private"}
         </h6> 
           <h6 style={{ color: "#3d3f72", marginLeft: "1px" }}>Priority:-
             {task.priority_id === 1 ? "High" : task.priority_id === 2 ? "Medium" : "Low"}
