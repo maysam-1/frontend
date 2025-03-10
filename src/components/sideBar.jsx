bbffd663fe056afd7a71232f6b081d43c2aada44
maysam-1 <maysamkh44@gmail.com>
2025-03-10 13:51:22 +0200

    added the song link

--- src/components/sideBar.jsx
+++ src/components/sideBar.jsx
@@ -7,6 +7,8 @@ import addGif from "../icons/add2.gif";
 import castleGif from "../icons/castle.gif"
 import HandleSignOut from "./signOut";
 import usersGif from "../icons/users.gif"
+import lightning from "../icons/lightning.png"
+
 function Sidebar() {
   const [active, setActive] = useState("Home");
   const [collapsed, setCollapsed] = useState(false);
@@ -16,9 +18,9 @@ function Sidebar() {
     { name: "Home", icon: <img src={homeGif} alt="Icon" style={{ height: "2rem", width: "2rem" }} />, path: "/home" }, 
     { name: "All Tasks", icon: <img src={pileGif} alt="Icon" style={{ height: "2rem", width: "2rem" }} />, path: "/all-tasks" }, 
     { name: "My Tasks", icon: <img src={mineGif} alt="Icon" style={{ height: "2rem", width: "2rem" }} />, path: "/mytasks" }, 
-    { name: "Add Task", icon: <img src={addGif} alt="Icon" style={{ height: "2rem", width: "2rem" }} />, path: "/add-task" }, 
-    { name: "THE OGs", icon: <img src={castleGif} alt="Icon" style={{ height: "2rem", width: "2rem" }} />, path: "/KK" }, 
-    { name: "View Users", icon: <img src={usersGif} alt="Icon" style={{ height: "2rem", width: "2rem" }} />, path: "/view-users" } // New button
+    { name: "Add Task", icon: <img src={addGif} alt="Icon" style={{ height: "2rem", width: "2rem" }} />, path: "/add-task" },  
+    { name: "View Users", icon: <img src={usersGif} alt="Icon" style={{ height: "2rem", width: "2rem" }} />, path: "/view-users" },
+    { name: "THE OGs", icon: <img src={castleGif} alt="Icon" style={{ height: "2rem", width: "2rem" }} />, path: "/KK" }
 
 
   ];
@@ -47,8 +49,22 @@ function Sidebar() {
           </button>
         ))}
         <HandleSignOut></HandleSignOut>
-        <label style={{position:"fixed",bottom:"5px",left:"5px"}}>Made By MAYSAM
-        </label>
+      
+        <a 
+          href="https://open.spotify.com/track/5vNRhkKd0yEAg8suGBpjeY?si=a669f2f0f1124622" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="apt-link"
+        >
+          APT. by ROSÉ & Bruno Mars
+          <img 
+            src={lightning} 
+            alt="Icon" 
+            style={{ height: "1.6rem", width: "1.6rem", marginLeft: "0.5rem" }} 
+          />
+        </a>
+
+
       </nav>
     </div>
   );
