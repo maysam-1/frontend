c25a0672efff1227093dd58ca660ed7c6ca8eba4
maysam-1 <maysamkh44@gmail.com>
2025-03-12 14:53:25 +0200

    added a display for username of the signed in user

--- src/components/topBar.js
+++ src/components/topBar.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function TopBar() {  
- 
+function TopBar() {
+  const [user, setUser] = useState(() => JSON.parse(localStorage.getItem("user")));
+
+  useEffect(() => {
+    const handleUserChange = () => {
+      setUser(JSON.parse(localStorage.getItem("user")));
+    };
+
+    window.addEventListener("userChange", handleUserChange);
+    return () => {
+      window.removeEventListener("userChange", handleUserChange);
+    };
+  }, []);
 
   return (
     <div>
       <div className="topbar">
-        <div style={{display:"flex",alignItems:"center"}}>
-          <h1 style={{color:"white"}}>APT. LIFE ORGANIZER</h1>
+      <h5 style={{ marginRight: "250px", color: "white",marginLeft:"50px" }}>
+            {user ? user.username : ""}
+          </h5>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <h1 style={{ color: "white" }}>APT. LIFE ORGANIZER</h1>
         </div>
       </div>
-
     </div>
   );
 }
